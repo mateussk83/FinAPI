87d2ecd2e78cf513fd1c8f334e051093b957698f
Mateus Garcia <mateussk83@gmail.com>
2022-12-08 07:57:43 -0300

    feat: check if cpf already exists

--- src/index.js
+++ src/index.js
@@ -12,13 +12,22 @@ app.use(express.json());
 // v4 = gera um numero totalmente aleatorio 
 app.post("/account", (request, response) => {
  const { cpf, name } = request.body;
- const id = uuidv4();
-
  
+ // some busca dentro do array e se a codição e dependendo da resposta ele retorna verdadeiro ou falso 
+ // neste caso ele verifica se tem algum cpf igual a este cpf nos dados
+ const customersAlreadyExists = customers.some(
+  (customer) => customer.cpf === cpf
+  );
+ if(customersAlreadyExists) {
+  // 400 bad request e a mensagem de erro
+  return response.status(400).json({error: "Customer Already Exists!"});
+ }
+
+ // adciona um novo {} no arrray customers
  customers.push({
   cpf,
   name,
-  id,
+  id: uuidv4(),
   statement: [],
  })
 
