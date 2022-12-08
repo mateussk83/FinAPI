0e57641e0cd7aa6af0c7188a45b803a4d80ff4e5
Mateus Garcia <mateussk83@gmail.com>
2022-12-08 16:54:22 -0300

    feat: search extract by date

--- src/index.js
+++ src/index.js
@@ -110,4 +110,17 @@ app.post("/withdraw",verifyIfExistsAccountCPF, (request, response) => {
  return response.status(201).send();
 })
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+ //chamando o customer do middleware verifiyIfExistsAccountCPF
+ const { customer } = request;
+ // query params = /
+ const { date } = request.query;
+
+// pegando a data 
+ const dateFormat = new Date(date + " 00:00");
+
+ const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+ return response.json(statement);
+})
+
 app.listen(3333);
