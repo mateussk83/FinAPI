e2dcfcbfda02c039d0af021a000a064154a03e2c
Mateus Garcia <mateussk83@gmail.com>
2022-12-08 07:44:36 -0300

    feat: finish first requirement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+
 const express = require("express");
+const { v4: uuidv4 } = require("uuid")
+
 
 const app = express()
+const customers = [];
+
+app.use(express.json());
+
+// body = string
+// v4 = gera um numero totalmente aleatorio 
+app.post("/account", (request, response) => {
+ const { cpf, name } = request.body;
+ const id = uuidv4();
+
+ 
+ customers.push({
+  cpf,
+  name,
+  id,
+  statement: [],
+ })
+
+ return response.status(201).send();
+})
 
 app.listen(3333);
