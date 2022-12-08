265d34ea34ff2642a74b94a4339c2c45796dad98
Mateus Garcia <mateussk83@gmail.com>
2022-12-08 16:20:16 -0300

    feat: make deposit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ const { v4: uuidv4 } = require("uuid")
 
 
 const app = express()
+
 const customers = [];
 
 app.use(express.json());
+
+// Middleware 
+// next ele que define se vai continuar ou vai parar 
+function verifyIfExistsAccountCPF(request, response, next) {
+ const { cpf } = request.headers;
+ // procurar dentro do array find e retornar a informação completa não só um true ou false 
+ const customer = customers.find( customer => customer.cpf === cpf);
+ // se nao tiver um customer
+ if(!customer) {
+  return response.status(400).json({error: "Customer not found!"})
+ }
+ // daqui por diante todos as rotas que usarem este middleware vai ter acesso este valor customer 
+ request.customer = customer;
+ return next();
+}
 
 // body = string
 // v4 = gera um numero totalmente aleatorio 
@@ -33,16 +49,31 @@ app.post("/account", (request, response) => {
 
  return response.status(201).send();
 })
+
 // aqui estamos passando o cpf pelo headers
-app.get("/statement", (request, response) => {
- const { cpf } = request.headers;
-// procurar dentro do array find e retornar a informação completa não só um true ou false 
- const customer = customers.find( customer => customer.cpf === cpf);
- // se nao tiver um customer
- if(!customer) {
-  return response.status(400).json({error: "Customer not found!"})
+// este segundo parametro recebe os middleware
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+ //chamando o customer do middleware verifiyIfExistsAccountCPF
+const { customer } = request;
+
+return response.json(customer.statement)
+})
+
+app.post("/deposit",verifyIfExistsAccountCPF, (request, response) => {
+ const { description, amount } = request.body;
+
+ const { customer } = request;
+
+ const statementOperation = {
+  description,
+  amount, 
+  created_at: new Date(),
+  type:"credit"
  }
- return response.json(customer.statement)
+
+ customer.statement.push(statementOperation)
+
+ return response.status(201).send();
 })
 
 app.listen(3333);
